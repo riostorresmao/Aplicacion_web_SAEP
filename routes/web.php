d7f177a611e3c0ea35dcb9ae4ac90afd4821fba8
KRISTIANSENA <90565251+KRISTIANSENA@users.noreply.github.com>
2022-08-18 15:53:53 -0500

    prueba  1.1
    prueba  1.1

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,12 @@ Route::resource('orders',OrdersController::class);
 Route::resource('routes',RoutesController::class);
 Route::resource('shoppinges',ShoppingController::class);
 
-//Route::middleware([
-    //'auth:sanctum',
-    //config('jetstream.auth_session'),
-    //'verified'
-//])->group(function () {
-    //Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');});
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
