679f206a396a66309f6bc63657b2d21b0bd7dbaf
riostorresmao <mauricio.rios0@misena.edu.co>
2022-08-18 23:26:22 -0500

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,48 +8,130 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Document</title>
 </head>
+<style>
+body {
+display: flex;
+min-height: 100vh;
+flex-direction: column;
+}
+
+main {
+flex: 1 0 auto;
+}
+
+body {
+   background-image: url("fondo.png");
+    background-size: cover;
+}
+
+.input-field input[type=date]:focus + label,
+.input-field input[type=text]:focus + label,
+.input-field input[type=email]:focus + label,
+.input-field input[type=password]:focus + label {
+color: #1e40e9;
+}
+
+.input-field input[type=date]:focus,
+.input-field input[type=text]:focus,
+.input-field input[type=email]:focus,
+.input-field input[type=password]:focus {
+border-bottom: 2px solid #1e51e9;
+box-shadow: none;
+}
+</style>
+</head>
+
 <body>
-    
-<div class="container">
-        <div class="row">
-            <div class="col s6 8 offset-s6">
-                <br><br><br><br>
-                <div class="card horizontal">
-                    <div class="card-stacked">
-                        <div class="card-content">
-                            <div class="col s6 offset-s3">
-                            <img class="responsive-img" src="img/login.png">
-                            </div>
+<div class="section"></div>
+<main>
+    <center>
+        <img class="responsive-img" style="width: 250px;" src="https://practipasta.weebly.com/uploads/3/1/5/6/31560323/img-20200108-wa0009_orig.jpg" />
+        <div class="section"></div>
 
-                            <form action="{{ route('login') }}" method="post">
-                                @csrf
-                            
-                                
+        <h5 class="indigo-text">Por favor ingrese la información</h5>
+        <div class="section"></div>
 
-                                <div class="row">
-                                    <div class="input-field col s12">
-                                        <i class="material-icons prefix">perm_identity</i>
-                                        <input id="email" type="email" name="email" class="validate" required>
-                                        <label for="email">Correo electronico</label>
-                                    </div>
-                                <div class="input-field col s12">
-                                        <i class="material-icons prefix">security</i>
-                                        <input id="password" type="password" name="password" class="validate" required>
-                                        <label for="password">Contraseña</label>
-                                    </div>
-                                </div>
-                                <div class="col s12  center-align">
-                                        <button type="submit" class="waves-effect waves-light btn darken-2">
-                                        <i class="material-icons right">input</i> iniciar sesión 
-                                        </button>
-                                </div>
-                            </form>
+        <div class="container">
+            <div class="z-depth-1 grey lighten-4 row" style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
+
+                <form action="{{ route('login') }}" method="post">
+                    @csrf
+                    <div class='row'>
+                        <div class='col s12'>
+                        </div>
+                    </div>
+
+                    <div class='row'>
+                        <div class='input-field col s12'>
+                            <input class='validate' type='email' name='email' id='email' />
+                            <label for='email'>Ingresa tu correo</label>
+                        </div>
+                    </div>
+
+                    <div class='row'>
+                        <div class='input-field col s12'>
+                            <input class='validate' type='password' name='password' id='password' />
+                            <label for='password'>Ingresa tu contraseña</label>
+                        </div>
+                        <label style='float: right;'>
+                            <a class='blue-text' href='#!'><b>olvidaste la contraseña?</b></a>
+                        </label>
+                    </div>
+
+                    <br />
+
+                    <center><BR>
+                        <div class='row'>
+                            <button type='submit' name='btn_login' class='col s12 btn btn-large waves-effect indigo'>Iniciar sesion</button>
+                        </div>
+                    </center>
+                </form>
+            </div>
+        </div>
+        <div class="fixed-action-btn horizontal click-to-toggle">
+            <a class="btn-floating btn-large red">
+                <i class="material-icons">menu</i>
+            </a>
+            <ul>
+                <li>
+                    <a class="waves-effect waves-light btn-floating social instagram"><i class="fa fa-instagram"> F</i></a>
+                </li>
+                <li>
+                    <a class="waves-effect waves-light btn-floating instagram"><i class="fa fa-instagram">T</i></a>
+                </li>
+                <li>
+                    <a class="waves-effect waves-light btn-floating instagram"><i class="fa fa-instagram">I</i></a>
+                </li>
+            </ul>
+        </div>
+
+    </center>
+    <center>
+        <footer class="footer" >
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <script>document.write(new Date().getFullYear())</script> &copy; Desarrollado por  <a href=""> SAEP GRUP</a>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="text-md-end footer-links d-none d-sm-block">
+                            <a href="javascript:void(0);">About Us</a>
+                            <a href="javascript:void(0);">Help</a>
+                            <a href="javascript:void(0);">Contact Us</a>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+            </foote
+    </center>
 
+    <div class="section"></div>
+    <div class="section"></div>
+</main>
+
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
 </body>
+
 </html>
